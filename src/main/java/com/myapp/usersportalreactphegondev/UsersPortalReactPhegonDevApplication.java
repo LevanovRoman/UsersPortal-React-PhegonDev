801ea6e885add6ab01341cbf314000deb5c24fd6
levanovroman <levanovroman2016@yandex.ru>
2024-08-02 15:42:04 +0300

    first commit

---
+++ src/main/java/com/myapp/usersportalreactphegondev/UsersPortalReactPhegonDevApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.usersportalreactphegondev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersPortalReactPhegonDevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsersPortalReactPhegonDevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/myapp/usersportalreactphegondev/UsersPortalReactPhegonDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.usersportalreactphegondev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersPortalReactPhegonDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
